b35c98a90424b760810e5a34d8f8b1d7473d3bbd
h1ut <amirosmonalievgg@gmail.com>
2023-01-30 20:15:22 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+//1
+
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+
+//   min(2, 6363)
+//   min(4234, -1)
+//   min(1, 53435)
+
+
+
+//2
+// function ggg(){
+//      let arr1 = prompt ('введите первое слово')
+//      let arr2 = prompt ('введите второе слово')
+//     if (arr1 > arr2)
+//     alert('первое слово больше второго ');
+//     else if (arr1 < arr2 )
+//     alert ('второе слово больше первого');
+// }
+// ggg()
+
+
+
+// 3
+
+
+
+
+
+// function js++ (){
+//     let a = prompt('введите слово  ')
+//     alert ('введенное слово имеет : ' + a.length + ' кодовых значений')
+// }
+//   js++()
+
+
+
+
+
+// dop
+
+
+
+// function dop (a,b,c){
+//  return (a + b + c );
+ 
+// }
+// let test = dop(1,2,3)
+// console.log(test)
